64a3004799e16b40ca1bc9186a3a7df9f5f7f70f
lukemolloy <luke_d_a@hotmail.com>
2016-05-05 02:16:05 +0000

    First push

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>SafeHaven.OurWomen</title>
+  <link rel="stylesheet" href="stylesheets/index.css">
+	<div class="navbar">
+	</div>
+</head>
+
+<body>
+	<div class="loginform">	
+		<form method="post" action="">
+			<center>
+			<p>
+				For our Women and children - an immediate, safe, secure, suportive, caring, free placement within a private home.
+			</p>
+			<label id="Username">Username</label> <input name="Username" type="text" /><br /><br />
+			<label id="Password">Password</label> <input name="Password" type="password" /><br /><br />
+			<input name="Signin" type="submit" value="Sign in" />
+			<input name="Register" type="submit" value="Register" />
+			</center>
+		</form>
+	</div>
+</body>
+
+<footer>
+<br />
+	Contact Us	|	About
+</footer>
+
+</html>
