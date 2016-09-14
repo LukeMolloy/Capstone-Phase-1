7dc985809dd17c5780aee39f723128164727718b
lukemolloy <luke_d_a@hotmail.com>
2016-09-14 08:25:45 +0000

    Luke

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@ require 'vendor/autoload.php';
 $sendgrid = new SendGrid('app55568313@heroku.com', 'zukzurbh9121');
 
 $email = new SendGrid\Email();
-$email->addTo('foo@bar.com')
+$email->addTo('luke.d.a.molloy@icloud.com')
     ->setFrom('me@bar.com')
     ->setSubject('Subject goes here')
     ->setText('Hello World!')
