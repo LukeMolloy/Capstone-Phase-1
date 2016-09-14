ad8b74009e17e986f4eaf9a77aeef0c31575976d
lukemolloy <luke_d_a@hotmail.com>
2016-09-14 05:09:21 +0000

    test

--- index.php
+++ index.php
@@ -95,10 +95,10 @@ $(document).ready(function(){
 <br />
 <table class="footertable">
 	<tr>
-		<td class="lightertext">SHOW</td>
-		<td class="lightertext">ABOUT</td>
-		<td class="lightertext">HELP</td>
-		<td class="lightertext">CONNECT</td>
+		<td class="lightertext">SHOW<br /><hr></td>
+		<td class="lightertext">ABOUT<br /><hr></td>
+		<td class="lightertext">HELP<br /><hr></td>
+		<td class="lightertext">CONNECT<br /><hr></td>
 	</tr>
 	<tr>
 		<td class="darkertext">
