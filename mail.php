2e3f1c5a588272bd91d5c7c7571e29ffd7e9285d
lukemolloy <luke_d_a@hotmail.com>
2016-09-14 08:30:04 +0000

    Luke

--- mail.php
+++ mail.php
@@ -4,10 +4,10 @@ $sendgrid = new SendGrid('app55568313@heroku.com', 'zukzurbh9121');
 
 $email = new SendGrid\Email();
 $email->addTo('luke.d.a.molloy@icloud.com')
-    ->setFrom('me@bar.com')
-    ->setSubject('Subject goes here')
-    ->setText('Hello World!')
-    ->setHtml('<strong>Hello World!</strong>');
+    ->setFrom('noreply@show.com')
+    ->setSubject('YOU GOT MAIL BITCH!')
+    ->setText('I cant believe this works!')
+    ->setHtml('<strong>THIS IS CRAZY</strong>');
 
 $sendgrid->send($email);
 
