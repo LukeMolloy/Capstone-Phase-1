127f5b427680616f1adcdcc6fac489830af3abe3
lukemolloy <luke_d_a@hotmail.com>
2016-10-05 05:26:57 +0000

    Justin

--- registration.php
+++ registration.php
@@ -84,7 +84,7 @@ function test_input($data) {
 	//	$sql->bindParam("last", $last);
 	//	$sql->bindParam("email", $email);
 	//	$sql->bindParam("hashpassword", $hashpassword);
-	//	$sql->execute();		
+		$sql->execute();		
 		
 		mysqli_query($db, $sql);
 
