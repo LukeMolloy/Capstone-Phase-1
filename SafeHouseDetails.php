0ab4c72e5ce275a9e918a6573d206713a0cd38d3
lukemolloy <luke_d_a@hotmail.com>
2016-10-06 06:53:49 +0000

    Justin

--- SafeHouseDetails.php
+++ SafeHouseDetails.php
@@ -84,14 +84,14 @@
                	if($result != NULL){
                	    while($row = $result->fetch_assoc()) {
                         echo "<table>
-                        <tr><th>First Name</th><td>".$row["FirstNameDecrypt"]."</td></tr><br /><br />
-                    	<tr><th>Last Name</th><td>".$row["LastNameDecrypt"]."</td></tr><br /><br />
-                        <tr><th>Phone Number</th><td>".$row["PhoneNumberDecrypt"]."</td></tr><br /><br />
-                        <tr><th>Email</th><td>".$row["EmailDecrypt"]."</td></tr><br /><br />
-                        <tr><th>Address</th><td>".$row["AddressDecrypt"]."</td></tr><br /><br />
-                        <tr><th>Suburb</th><td>".$row["SuburbDecrypt"]."</td></tr><br /><br />
-                        <tr><th>City</th><td>".$row["CityDecrypt"]."</td></tr><br /><br />
-                        <tr><th>State</th><td>".$row["StateDecrypt"]."</td></tr>
+                        <tr><th>First Name</th><td>".$row["FirstNameDecrypt"]."</td><td><input type='text' name='editname'></td></tr><br /><br />
+                    	<tr><th>Last Name</th><td>".$row["LastNameDecrypt"]."</td><td><input type='text' name='editname'></td></tr><br /><br />
+                        <tr><th>Phone Number</th><td>".$row["PhoneNumberDecrypt"]."</td><td><input type='text' name='editname'></td></tr><br /><br />
+                        <tr><th>Email</th><td>".$row["EmailDecrypt"]."</td><td><input type='text' name='editname'></td></tr><br /><br />
+                        <tr><th>Address</th><td>".$row["AddressDecrypt"]."</td><td><input type='text' name='editname'></td></tr><br /><br />
+                        <tr><th>Suburb</th><td>".$row["SuburbDecrypt"]."</td><td><input type='text' name='editname'></td></tr><br /><br />
+                        <tr><th>City</th><td>".$row["CityDecrypt"]."</td><td><input type='text' name='editname'></td></tr><br /><br />
+                        <tr><th>State</th><td>".$row["StateDecrypt"]."</td><td><input type='text' name='editname'></td></tr>
                         </table>";
                         
                         
