509ea94f63d3be213feff53cc04f8fc646f76c29
lukemolloy <luke_d_a@hotmail.com>
2016-10-06 05:08:18 +0000

    Justin

--- checkloginsafehaven.php
+++ checkloginsafehaven.php
@@ -10,7 +10,7 @@
 				$myusername=$_POST['Username'];
 				$password=$_POST['Password']; 
 				
-								
+				VALUES (AES_ENCRYPT('$first', 'show2016'), AES_ENCRYPT('$last', 'show2016'), AES_ENCRYPT('$phonenumber', 'show2016'), AES_ENCRYPT('$email', 'show2016'), '$hashpassword', AES_ENCRYPT('$address', 'show2016'), AES_ENCRYPT('$suburb', 'show2016'), AES_ENCRYPT('$postcode', 'show2016'), AES_ENCRYPT('$city', 'show2016'), AES_ENCRYPT('$state', 'show2016'), '$spacesavailable', '$infantspaces', '$petfriendly', '$bluecardnumber', '$description', '$gender', '$nights')";				
 
 			//	$salt = uniqid(mt_rand(), true);
 	
@@ -18,7 +18,7 @@
 			
 				$hashpassword = hash("sha256", $password);
 		
-				$sql="SELECT * FROM House WHERE Email='$myusername' and Password='$hashpassword'";
+				$sql="SELECT * FROM House WHERE Email= AES_ENCRYPT('$myusername', 'show2016') and Password= AES_ENCRYPT('$hashpassword', 'show2016')";
 				$result = mysqli_query($db, $sql);
 				$count = mysqli_num_rows($result);
 				
