e1e8ff3f5111e8b4fa4189dc5dc277841a470a42
lukemolloy <luke_d_a@hotmail.com>
2016-10-06 05:10:01 +0000

    Justin

--- checkloginsafehaven.php
+++ checkloginsafehaven.php
@@ -10,7 +10,7 @@
 				$myusername=$_POST['Username'];
 				$password=$_POST['Password']; 
 				
-				VALUES (AES_ENCRYPT('$first', 'show2016'), AES_ENCRYPT('$last', 'show2016'), AES_ENCRYPT('$phonenumber', 'show2016'), AES_ENCRYPT('$email', 'show2016'), '$hashpassword', AES_ENCRYPT('$address', 'show2016'), AES_ENCRYPT('$suburb', 'show2016'), AES_ENCRYPT('$postcode', 'show2016'), AES_ENCRYPT('$city', 'show2016'), AES_ENCRYPT('$state', 'show2016'), '$spacesavailable', '$infantspaces', '$petfriendly', '$bluecardnumber', '$description', '$gender', '$nights')";				
+				//VALUES (AES_ENCRYPT('$first', 'show2016'), AES_ENCRYPT('$last', 'show2016'), AES_ENCRYPT('$phonenumber', 'show2016'), AES_ENCRYPT('$email', 'show2016'), '$hashpassword', AES_ENCRYPT('$address', 'show2016'), AES_ENCRYPT('$suburb', 'show2016'), AES_ENCRYPT('$postcode', 'show2016'), AES_ENCRYPT('$city', 'show2016'), AES_ENCRYPT('$state', 'show2016'), '$spacesavailable', '$infantspaces', '$petfriendly', '$bluecardnumber', '$description', '$gender', '$nights')";				
 
 			//	$salt = uniqid(mt_rand(), true);
 	
