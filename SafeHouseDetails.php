335c9a814bf39abb4654ae76ddd970e109896261
lukemolloy <luke_d_a@hotmail.com>
2016-10-07 05:47:09 +0000

    Justin

--- SafeHouseDetails.php
+++ SafeHouseDetails.php
@@ -89,7 +89,7 @@
                	    while($row = $result->fetch_assoc()) {
                         echo "
                         <table>
-                        <tr><th>Spaces Available: </th><td><input type='text' id='number' value=".$row["Spacesavailable"]." size='2' readonly></td><td><button class='updatebutton' onclick='add();'>&nbsp + &nbsp</button> <button class='updatebutton' onclick='minus();'>&nbsp - &nbsp</button></td></tr>
+                        <tr><th>Spaces Available: </th><td><input type='text' id='number' value=".$row["Spacesavailable"]." size='2' readonly></td><td><button type='button' class='updatebutton' onclick='add();'>&nbsp + &nbsp</button> <button class='updatebutton' type='button' onclick='minus();'>&nbsp - &nbsp</button></td></tr>
                         
                         </table>";
                         
