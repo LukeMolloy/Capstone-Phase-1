f47f765a99734c12f046cb0f49f89ab4dad5ee48
lukemolloy <luke_d_a@hotmail.com>
2016-10-11 06:40:38 +0000

    Justin

--- SafeHouseDetails.php
+++ SafeHouseDetails.php
@@ -98,14 +98,14 @@
                         <form method='POST'><table>
                         <tr><th>Spaces Available: </th><td><input type='text' id='number' value=".$row["Spacesavailable"]." size='2' readonly></td><td><button type='button' class='updatebutton' onclick='add();'>&nbsp + &nbsp</button> <button class='updatebutton' type='button' onclick='minus();'>&nbsp - &nbsp</button></td></tr>
                        
-                        
+                        $amount++;
                         
                        
                	                        <input type='submit' value='Update' name='submit' class='updatebutton' id='submit'></table></form>";
                	    }
                	    if(isset($_POST['submit'])){
                	    	
-               	$amount++;
+     
 							$dothis = mysqli_query($db, $stmt);
 						}
                	}
