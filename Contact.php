9be9010ed160b0666fc076320a24ee2fb50eee2c
lukemolloy <luke_d_a@hotmail.com>
2016-10-12 08:30:46 +0000

    Luke

--- Contact.php
+++ Contact.php
@@ -10,7 +10,6 @@
 <body>
 <div class="header">
 	<label class="title">Safe Haven Our Women</label>
-		<ul>
 			<li><a href="/About.php">About Us</a></li>
 			<li><a href="/Contact.php">Contact Us</a></li>
 			<li><a href="/index.php">Home</a></li>
