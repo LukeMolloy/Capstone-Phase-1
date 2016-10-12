3a7be79e88b6987df5bcf2ec332ec2b8fb0b8d0f
lukemolloy <luke_d_a@hotmail.com>
2016-10-12 03:51:33 +0000

    Justin

--- SafeHouseDetails.php
+++ SafeHouseDetails.php
@@ -82,9 +82,13 @@
         
         <table cellspacing="100">
     		<td valign="top">
+    			<h2>Find and edit your details below:</h2>
+    			</td>
+    			<td>
+    			
     			<?php 
         
-       echo "<h2>Find and edit your details below:</h2>";
+    	
         
         
         $sql="SELECT CAST(AES_DECRYPT(FirstName, 'show2016') AS CHAR(50)) FirstNameDecrypt, CAST(AES_DECRYPT(LastName, 'show2016') AS CHAR(50)) LastNameDecrypt, CAST(AES_DECRYPT(PhoneNumber, 'show2016') AS CHAR(50)) PhoneNumberDecrypt, CAST(AES_DECRYPT(Email, 'show2016') AS CHAR(50)) EmailDecrypt, CAST(AES_DECRYPT(Address, 'show2016') AS CHAR(50)) AddressDecrypt, CAST(AES_DECRYPT(Suburb, 'show2016') AS CHAR(50)) SuburbDecrypt, CAST(AES_DECRYPT(Postcode, 'show2016') AS CHAR(50)) PostcodeDecrypt, CAST(AES_DECRYPT(City, 'show2016') AS CHAR(50)) CityDecrypt, CAST(AES_DECRYPT(State, 'show2016') AS CHAR(50)) StateDecrypt, Spacesavailable, Petfriendly, Gendertaken, Authorised, Nights FROM House 
@@ -128,6 +132,8 @@
         
 //sdfsd
 ?>
+</td>
+</table>
 </div>
 
 
