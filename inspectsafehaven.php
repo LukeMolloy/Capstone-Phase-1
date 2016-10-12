6295e894bcc37888cee57ca7191a6890bd6296a2
lukemolloy <luke_d_a@hotmail.com>
2016-10-12 08:47:39 +0000

    Justin

--- inspectsafehaven.php
+++ inspectsafehaven.php
@@ -138,14 +138,6 @@
 </div>
 
 
-<!-- The Modal -->
-<div id="myModal" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">x</span>
-    <p>Some text in the Modal..</p>
-  </div>
 
 </div>
 
