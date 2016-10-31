794ed1ccd2668e9d0ad1597a592bdb7f724ef177
lukemolloy <luke_d_a@hotmail.com>
2016-10-31 04:15:04 +0000

    Justin

--- inspectsafehaven.php
+++ inspectsafehaven.php
@@ -10,12 +10,15 @@
 	    header("Location: index.php");
 	}
 ?>	
-   
+
+
+	<script type="text/javascript" src="/popups.js"></script>
     <link href='//fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 	<title>SafeHaven.OurWomen</title>
 	<link rel="stylesheet" href="stylesheets/templatetest.css">
   
+    <link rel="stylesheet" href="stylesheets/popups.css">
 	<div class="navbar">
 	</div>
 	<?php include 'dbLogin.php' ?>
@@ -50,20 +53,48 @@
 		<table>
 			<td>
 				
-<div id="map" style="width:100%;height:500px"></div>
+				<button onclick="getLocation()">Try It</button>
+
+<div id="mapholder"></div>
 
 <script>
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(51.5, -0.2), 
-    zoom: 10
-  }
-  var map = new google.maps.Map(mapCanvas, mapOptions);
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
+    +latlon+"&zoom=14&size=400x300&sensor=false";
+    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+}
+
+function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
 }
 </script>
-
-<script src="https://maps.googleapis.com/maps/api/js?callback=myMap"></script>
+				
+				
 				
 				<div id="article">
             		<!-- Place the map here -->  
