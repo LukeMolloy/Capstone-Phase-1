66eeefb613d83d348954f185f5240d971d30e5d3
lukemolloy <luke_d_a@hotmail.com>
2016-10-31 03:56:33 +0000

    Justin

--- inspectsafehaven.php
+++ inspectsafehaven.php
@@ -59,6 +59,22 @@
     <div class="loginform">
 		<table>
 			<td>
+				
+<div id="map" style="width:100%;height:500px"></div>
+
+<script>
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.2), 
+    zoom: 10
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?callback=myMap"></script>
+				
 				<div id="article">
             		<!-- Place the map here -->  
         		</div>
