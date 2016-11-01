ae31b71167bccf6dd530dc0c226f730c73aec919
lukemolloy <luke_d_a@hotmail.com>
2016-11-01 01:12:54 +0000

    Justin

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
 <title>SafeHaven.Community</title>
 <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 
- 	<link rel="stylesheet" href="stylesheets/indextest.css">
- 	<link rel="stylesheet" href="stylesheets/templatetest.css">
+ 	<!--<link rel="stylesheet" href="stylesheets/indextest.css">-->
+ 	<link rel="stylesheet" href="stylesheets/indextemplate.css">
 	<?php include 'dbLogin.php' ?>
 </head>
 <body>
