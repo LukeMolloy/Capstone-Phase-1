a554d7d1f19890dd360b0cbc0049f30d47882ded
lukemolloy <luke_d_a@hotmail.com>
2016-11-01 03:06:56 +0000

    Justin

--- searchsafehaven.php
+++ searchsafehaven.php
@@ -8,18 +8,16 @@ session_start();
 
 <?php 
     ob_start();
-    
-    
-    $suburb = $_POST['AES_ENCRYPT('$first', 'Suburb')'];
+    $suburb = $_POST['Suburb'];
     $_SESSION['Suburb']=$suburb;
     
-    $postcode = $_POST['AES_ENCRYPT('$first', 'Postcode')'];
+    $postcode = $_POST['Postcode'];
     $_SESSION['Postcode']=$postcode;    
     
-    $city = $_POST['AES_ENCRYPT('$first', 'City')'];
+    $city = $_POST['City'];
     $_SESSION['City']=$city;
     
-    $state = $_POST['AES_ENCRYPT('$first', 'State')'];
+    $state = $_POST['State'];
     $_SESSION['State']=$state;
     
     header("location:safehaven.php");
