8cc16d3ef58609404505bb97969726e59863a2e7
lukemolloy <luke_d_a@hotmail.com>
2016-11-02 02:49:53 +0000

    Justin

--- inspectsafehaven.php
+++ inspectsafehaven.php
@@ -137,7 +137,7 @@ function myMap() {
   var map = new google.maps.Map(mapCanvas, mapOptions);
 }
 </script>
-<script src="https://maps.googleapis.com/maps/api/js?callback=myMap"></script>
+
 
 <footer>
 <br />
